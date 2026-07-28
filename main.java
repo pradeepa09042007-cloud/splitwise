b0e2ec4e7ab6e3c4d2547bd651fd401f3884a8a4
PRADEEP A 518 <pradeepa09042007@gmail.com>
2026-07-28 14:22:18 +0530

    1.2

--- main.java
+++ main.java
@@ -3,6 +3,10 @@ package splitwiseapp;
 public class Main{
     public static void main(String[] args) {
         System.out.println("Splitwise ");
+        double totalamount =500;
+        System.out.println(totalamt);
+        int frndscount = 3;
+        System.out.println(numfrnds);
        
 
     }
