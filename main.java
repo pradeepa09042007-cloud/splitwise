7097384b6ad670e8396fabccf6674d6a5b3a00b7
PRADEEP A 518 <pradeepa09042007@gmail.com>
2026-07-28 14:38:20 +0530

    1.8

--- main.java
+++ main.java
@@ -1,15 +1,19 @@
 package splitwiseapp;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main{
     public static void main(String[] args) {
 
          Scanner sc = new Scanner(System.in);
+         ArrayList<String> friendsList = new ArrayList<>();
        
    boolean runstate = true;
         while(runstate==true) {
             System.out.println("====Menu====");
             System.out.println("1. Record Expense");
+             System.out.println("2. Add friends");
+            System.out.println("3. List friends ");
             System.out.println("0. Quit");
             System.out.print("choice: ");
             int choice =  sc.nextInt();
@@ -27,10 +31,27 @@ public class Main{
                         double totalamt = sc.nextDouble();
                         String expLine = payerName + " paid :" + totalamt;
                         System.out.println(expLine);
-                        int numfrnds = 3;
+                         int numfrnds = friendsList.size();
                         double personshare = totalamt / numfrnds;
                         String shareLine = "each person pays :" + personshare;
                         System.out.println(shareLine);
+                     }
+                }
+
+                case 2 ->{
+                    System.out.println("Enter friend name:");
+                    String friendName = sc.nextLine();
+                    friendsList.add(friendName);
+                    System.out.println("added " + friendName);
+                }
+
+                case 3 ->{
+                    if(friendsList.isEmpty()) System.out.println("No friends added yet.Add friends first!");
+                    else{
+                        for(String name:friendsList){
+                            System.out.println("-" + name);
+                        }
+                    }
                 }
 
                 case 0 ->{
