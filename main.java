70df869a7584474d5e8b9a35875948a691c7e2c6
PRADEEP A 518 <pradeepa09042007@gmail.com>
2026-07-28 14:26:53 +0530

    1.3

--- main.java
+++ main.java
@@ -3,10 +3,18 @@ package splitwiseapp;
 public class Main{
     public static void main(String[] args) {
         System.out.println("Splitwise ");
+
+        String name = "max";
+        System.out.println(name);
+
+        
         double totalamount =500;
         System.out.println(totalamt);
+
+        
         int frndscount = 3;
         System.out.println(numfrnds);
+        
        
 
     }
